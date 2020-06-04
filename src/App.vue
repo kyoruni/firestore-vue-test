e12b56c0edf4da18f3c221d8e6e8357015f0cf9b
kyoruni <kyoruni@gmail.com>
2020-06-04 19:04:23 +0900

    :recycle: 不要なファイルを削除

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <template>
   <div id="app">
     <div class="btn btn-info">hoge</div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
